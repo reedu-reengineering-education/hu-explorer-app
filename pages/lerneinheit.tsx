c204b9d05f5ae39cb9dc872c3c9930647e3e6788
Matthias Pfeil <m.pfeil@reedu.de>
2021-11-04 15:32:14 +0100

    Add flex layout

--- pages/lerneinheit.tsx
+++ pages/lerneinheit.tsx
@@ -1,5 +1,15 @@
 function Lerneinheit() {
-  return <div>Lerneinheit</div>;
+  return (
+    <div className="flex flex-col">
+      <div className="flex flex-auto">
+        <h1>Title</h1>
+      </div>
+      <div className="flex flex-col sm:flex-row ">
+        <div className="flex-grow md:w-2/3 bg-red-400">Tabelle</div>
+        <div className="flex-none md:w-1/3 bg-green-400">Auswertung</div>
+      </div>
+    </div>
+  );
 }
 
 export default Lerneinheit;
