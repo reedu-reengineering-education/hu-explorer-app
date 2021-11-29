d968ac2e3998eb7411b3c0ade72bce69a43f683d
Felix Erdmann <hello@felixerdmann.com>
2021-11-29 16:51:46 +0100

    add sample spreadsheet

--- pages/lerneinheit/biodiversitaet.tsx
+++ pages/lerneinheit/biodiversitaet.tsx
@@ -1,7 +1,46 @@
+import React, { useState } from 'react';
+import Spreadsheet, { CellBase, Matrix } from 'react-spreadsheet';
+
 import { useLerneinheitParams } from '@/hooks/useLerneinheitParams';
 
 const Biodiversitaet = () => {
   const { schule } = useLerneinheitParams();
+
+  const [data, setData] = useState<Matrix<CellBase>>([
+    [
+      { value: 'Versiegelung', readOnly: true, className: 'font-bold text-xl' },
+      { value: '' },
+    ],
+    [
+      {
+        value: 'Versiegelungsgrad in %',
+        readOnly: true,
+      },
+      { value: 0 },
+    ],
+    [{ value: '', readOnly: true }],
+    [
+      {
+        value: 'Artenvielfalt',
+        readOnly: true,
+        className: 'font-bold text-xl',
+      },
+      { value: '' },
+    ],
+    [
+      {
+        value: 'Tier',
+        readOnly: true,
+      },
+      { value: 'Anzahl', readOnly: true },
+    ],
+    [
+      {
+        value: '',
+      },
+      { value: '' },
+    ],
+  ]);
 
   return (
     <div className="flex flex-col">
@@ -10,7 +49,9 @@ const Biodiversitaet = () => {
         <div className="font-semibold text-gray-500">{schule}</div>
       </div>
       <div className="flex flex-col sm:flex-row divide-x-2 divide-blue-500">
-        <div className="flex-grow md:w-2/3 p-4">Tabelle</div>
+        <div className="flex-grow md:w-2/3 p-4">
+          <Spreadsheet data={data} onChange={setData} />
+        </div>
         <div className="flex-none md:w-1/3 p-4">Auswertung</div>
       </div>
     </div>
