281455ce615e64171eee481d9dfde76a52419227
Felix Erdmann <hello@felixerdmann.com>
2021-12-14 15:37:34 +0100

    update tile style

--- components/Tile.tsx
+++ components/Tile.tsx
@@ -14,18 +14,23 @@ const Tile = ({ title, min, max, color }: TileProps) => {
         color.bg ?? 'bg-blue-600'
       } text-white shadow-lg ${
         color.shadow ?? 'shadow-blue-100'
-      } text-center aspect-square w-32 h-32 m-2`}
+      } text-center aspect-square w-36 h-36 m-2`}
     >
       <div className="p-4 flex flex-col justify-between h-full">
         <span className="font-semibold text-xl">{title}</span>
+        <hr></hr>
         <div className="flex flex-col">
           <span className="flex items-center justify-evenly">
             <VolumeUpIcon className="h-3 w-3" />
-            {min ? min : '-'} dB
+            <div>
+              <span className="text-3xl font-light">{min ?? '-'}</span> dB
+            </div>
           </span>
-          <span className="flex items-center  justify-evenly">
+          <span className="flex items-center justify-evenly">
             <VolumeUpIcon className="h-5 w-5" />
-            {max ? max : '-'} dB
+            <div>
+              <span className="text-3xl font-light">{max ?? '-'}</span> dB
+            </div>
           </span>
         </div>
       </div>
