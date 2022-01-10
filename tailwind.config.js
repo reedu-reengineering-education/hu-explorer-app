ec36c2afb418ef9716936831c5a29a1aff73d85e
Felix Erdmann <hello@felixerdmann.com>
2022-01-10 13:05:09 +0100

    add he colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,41 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        he: {
+          blue: {
+            light: '#5394d0',
+            DEFAULT: '#004c90',
+          },
+          green: {
+            light: '#bad580',
+            DEFAULT: '#6bbe98',
+          },
+          aqua: {
+            DEFAULT: '#56bfc6',
+          },
+          lilac: {
+            DEFAULT: '#7d8bc5',
+          },
+          violet: {
+            DEFAULT: '#9d78b4',
+          },
+          magenta: {
+            DEFAULT: '#e175ac',
+          },
+          red: {
+            DEFAULT: '#ec656b',
+          },
+          orange: {
+            DEFAULT: '#f6a03e',
+          },
+          yellow: {
+            DEFAULT: '#fbbd35',
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
