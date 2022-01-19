93ab406e9c27a1faaf229caef4b4ac845b2b9293
Matthias Pfeil <m.pfeil@reedu.de>
2022-01-19 15:00:24 +0100

    Extend expitions parameters

--- hooks/useExpeditionParams.ts
+++ hooks/useExpeditionParams.ts
@@ -3,12 +3,13 @@ import { useRouter } from 'next/dist/client/router';
 type ExpeditionParams = {
   schule: string | string[];
   gruppe: string | string[];
+  daten: string | string[];
 };
 
 export const useExpeditionParams = (): ExpeditionParams => {
   const router = useRouter();
 
-  const { schule, gruppe } = router.query;
+  const { schule, gruppe, daten } = router.query;
 
-  return { schule, gruppe };
+  return { schule, gruppe, daten };
 };
